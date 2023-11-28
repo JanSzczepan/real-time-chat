21517e4b776db48118cf267cf23fe376f3ce0152
Jan Szczepan <janszczepan62@gmail.com>
2023-11-28 12:34:16 +0100

    Add join room functionality

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,35 @@
+import { useState } from 'react'
+import { HubConnectionBuilder, LogLevel, HubConnection } from '@microsoft/signalr'
+import { Lobby } from './components'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+    const [, setConnection] = useState<HubConnection>()
+
+    const joinRoom = async (user: string, room: string) => {
+        try {
+            const connection = new HubConnectionBuilder()
+                .withUrl('http://localhost:5110/chat')
+                .configureLogging(LogLevel.Information)
+                .build()
+
+            connection.on('ReceiveMessage', (user, message) => {
+                console.log('message received: ', message, 'user: ', user)
+            })
+
+            await connection.start()
+            await connection.invoke('JoinRoom', { user, room })
+            setConnection(connection)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
-        <div>
-            <span>Hello World</span>
+        <div className='app'>
+            <h2>MyChat</h2>
+            <Lobby joinRoom={joinRoom} />
         </div>
     )
 }
