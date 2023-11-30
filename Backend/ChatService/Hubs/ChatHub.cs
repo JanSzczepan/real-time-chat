d5602d0791f1c08c997cb4be6cd49c77f9810ebc
Jan Szczepan <janszczepan62@gmail.com>
2023-11-30 12:36:06 +0100

    Add method for sending message

--- Backend/ChatService/Hubs/ChatHub.cs
+++ Backend/ChatService/Hubs/ChatHub.cs
@@ -5,10 +5,17 @@ namespace ChatService.Hubs;
 public class ChatHub : Hub
 {
     private const string ChatBot = "MyChat Bot";
+    private readonly IDictionary<string, UserConnection> _connections;
+
+    public ChatHub(IDictionary<string, UserConnection> connections)
+    {
+        _connections = connections;
+    }
 
     public async Task JoinRoom(UserConnection userConnection)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+        _connections[Context.ConnectionId] = userConnection;
         await Clients
             .Group(userConnection.Room)
             .SendAsync(
@@ -16,5 +23,15 @@ public class ChatHub : Hub
                 ChatBot,
                 $"{userConnection.User} has joined {userConnection.Room}"
             );
+    }
+
+    public async Task SendMessage(string message)
+    {
+        if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+        {
+            await Clients
+                .Group(userConnection.Room)
+                .SendAsync("ReceiveMessage", userConnection.User, message);
+        }
     }
 }
