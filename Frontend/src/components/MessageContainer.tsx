a0d7638e418b3d2d5ee2b2b9855dd9206cc2f033
Jan Szczepan <janszczepan62@gmail.com>
2023-11-30 11:23:51 +0100

    Create MessageContainer component for displaying messages

---
+++ Frontend/src/components/MessageContainer.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from 'react'
+import { MessageInfo } from '../ts/global'
+
+type MessageContainerProps = {
+    messages: MessageInfo[]
+}
+
+const MessageContainer = ({ messages }: MessageContainerProps) => {
+    const messageRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (messageRef && messageRef.current) {
+            const { scrollHeight, clientHeight } = messageRef.current
+            messageRef.current.scrollTo({ left: 0, top: scrollHeight - clientHeight, behavior: 'smooth' })
+        }
+    }, [messages])
+
+    return (
+        <div
+            ref={messageRef}
+            className='message-container'
+        >
+            {messages.map((m, index) => (
+                <div
+                    key={index}
+                    className='user-message'
+                >
+                    <div className='message bg-primary'>{m.message}</div>
+                    <div className='from-user'>{m.user}</div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default MessageContainer
